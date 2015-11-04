2fa0ce888dcd8700c6766eabf01d80aff0728b95
seemonz <seemonz@gmail.com>
2015-11-04 13:01:01 -0800

    added cityName to temp and render

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -3,16 +3,17 @@ $(function() {
   var sanFranEL = $("#sanfrancisco");
   var weatherTemp = $('#weather-template').html();
   // var weatherRendered = Mustache.render(weatherTemp, {tempC: sanFranTempC});
-  
+
   // SanFrancisco Current Observations API get
   $('.weather').on('click', function() {
     var weatherApi = 'http://api.wunderground.com/api/626ed9352fa3920c/conditions/q/CA/San_Francisco.json';
     $.getJSON(weatherApi, {
     })
       .success( function(data) {
+        var cityName = data.current_observation.display_location.full;
         var sanFranTempC = data.current_observation.temp_c;
-        var weatherRendered = Mustache.render(weatherTemp, {tempC: sanFranTempC});
-        $('#sanfrancisco').html(weatherRendered);
+        var weatherRendered = Mustache.render(weatherTemp, {tempC: sanFranTempC, cityName: cityName});
+        $('#sanfrancisco').replaceWith(weatherRendered);
         // $('<p>').text(sanFranTempC).appendTo(sanFranEL);
       });
     });

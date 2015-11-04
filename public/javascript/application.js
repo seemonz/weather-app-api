9b5e37dab721da15247ff02459b00001af16b1ca
seemonz <seemonz@gmail.com>
2015-11-04 12:50:19 -0800

    added basic mustache temp

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,9 @@
 $(function() {
   // selectors
   var sanFranEL = $("#sanfrancisco");
-
+  var weatherTemp = $('#weather-template').html();
+  // var weatherRendered = Mustache.render(weatherTemp, {tempC: sanFranTempC});
+  
   // SanFrancisco Current Observations API get
   $('.weather').on('click', function() {
     var weatherApi = 'http://api.wunderground.com/api/626ed9352fa3920c/conditions/q/CA/San_Francisco.json';
@@ -9,7 +11,9 @@ $(function() {
     })
       .success( function(data) {
         var sanFranTempC = data.current_observation.temp_c;
-        $('<p>').text(sanFranTempC).appendTo(sanFranEL);
+        var weatherRendered = Mustache.render(weatherTemp, {tempC: sanFranTempC});
+        $('#sanfrancisco').html(weatherRendered);
+        // $('<p>').text(sanFranTempC).appendTo(sanFranEL);
       });
     });
 
